b18ca0ee62e0a4cb8b892519a0a8b3e6d2b144ce
Kenber <kenberspeng@gmail.com>
2014-10-23 21:24:19 +0800

    First commit:runable with opencv.

---
+++ Resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by VideoPlayer.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_VIDEOPLAYER_DIALOG          102
+#define IDR_MAINFRAME                   128
+#define IDI_ICON1                       129
+#define IDI_ICON2                       130
+#define IDC_BUTTON_PLAY                 1000
+#define IDC_BUTTON_PAUSE                1001
+#define IDC_SLIDER                      1003
+#define IDC_OCX1                        1006
+#define IDC_BUTTON_OPENFILES            1007
+#define IDC_VIDEO_SHOW                  1009
+#define IDC_EDIT_OPENFILES              1010
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
---
+++ VideoPlayerDlg.h
@@ -0,0 +1,42 @@
+// VideoPlayerDlg.h : 头文件
+//
+
+#pragma once
+#include "afxcmn.h"
+#include "afxwin.h"
+
+
+// CVideoPlayerDlg 对话框
+class CVideoPlayerDlg : public CDialog
+{
+// 构造
+public:
+	CVideoPlayerDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_VIDEOPLAYER_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButtonPlay();
+	afx_msg void OnBnClickedButtonPause();
+	afx_msg void OnBnClickedCancel();
+	afx_msg void OnNMCustomdrawSlider(NMHDR *pNMHDR, LRESULT *pResult);
+	afx_msg void OnBnClickedButtonOpenfiles();
+	CSliderCtrl m_slider;
+	CButton m_openfiles;
+	
+};
